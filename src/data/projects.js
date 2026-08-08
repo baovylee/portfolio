050831636e874149154f817521496b2c2c69e0bd
Vy Bao Le <lebaovy596@gmail.com>
2026-08-08 15:40:43 +0700

    data and hooks created for logic and design

---
+++ src/data/projects.js
@@ -0,0 +1,36 @@
+export const projectsData = [
+  {
+    title: "Bit Clicks",
+    description:
+      "Bit Clicks is a web app for researchers and clinicians that visualizes biomedical OMICS data. Users upload their raw data, select visualization parameters and download their dynamically generated plots.",
+    techStack: [],
+    image: "/biomex.png",
+  },
+  {
+    title: "Toronto and Vancouver Crime Analysis",
+    description:
+      "Toronto and Vancouver Crime Analysis is a project that analyzes crime data in two major Canadian cities. It provides insights into crime trends, hotspots, and patterns to help inform policy decisions.",
+    techStack: [],
+    image: "/crimeanalysis1.gif",
+    githubLink: "https://github.com/baovylee/Vancouver-and-Toronto-Crime-Analysis",
+    appLink: "https://vancouver-and-toronto-crime-analysis.streamlit.app/",
+  },
+  {
+    title: "Time Management",
+    description:
+      "A full-stack productivity app that helps users manage tasks, track focus sessions with the Pomodoro technique, and gain AI-powered productivity insights.",
+    techStack: ["Tech 1", "Tech 2", "Tech 3", "Tech 4", "Tech 5"],
+    image: "/timemanagement.gif",
+    githubLink: "https://github.com/baovylee/time-management",
+    appLink: "https://time-forge-frontend.onrender.com/",
+  },
+  {
+    title: "Portfolio",
+    description:
+      "A personal portfolio website that showcases my projects, skills, and experiences. It is built with modern web technologies and features a responsive design.",
+    techStack: ["Tech 1", "Tech 2", "Tech 3", "Tech 4", "Tech 5"],
+    image: "/favicon.svg",
+    githubLink: "https://github.com/baovylee/portfolio",
+    appLink: "https://baovylee.github.io/portfolio/",
+  }
+];
